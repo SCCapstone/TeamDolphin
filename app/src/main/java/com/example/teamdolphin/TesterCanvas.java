a7b1817af63532d3fafe9826d6cb5ca844ac3cc4
CanalesLuis <lcanals217@gmail.com>
2021-12-02 10:43:22 -0500

    slight theme change to the canvas, deleted two files from previous canvas attempts.

--- app/src/main/java/com/example/teamdolphin/TesterCanvas.java
+++ app/src/main/java/com/example/teamdolphin/TesterCanvas.java
@@ -2,6 +2,7 @@ package com.example.teamdolphin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 
@@ -13,6 +14,7 @@ import android.provider.MediaStore;
 import android.view.View;
 import android.view.ViewTreeObserver;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 
