a418fee3f28017bdaee7b6bdaf31ee20efa697d0
TysonMedlin <giftcard10@gmail.com>
2022-01-16 14:01:09 -0500

    Added User Interaction Tests via the Expresso package, and added an example Junit test.

---
+++ app/src/androidTest/java/com/example/teamdolphin/TestUndo.kt
@@ -0,0 +1,93 @@
+package com.example.teamdolphin
+
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.filters.LargeTest
+import androidx.test.rule.ActivityTestRule
+import androidx.test.runner.AndroidJUnit4
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers.allOf
+import org.hamcrest.TypeSafeMatcher
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class TestUndo {
+
+    @Rule
+    @JvmField
+    var mActivityTestRule = ActivityTestRule(MainActivity::class.java)
+
+    @Test
+    fun testUndo() {
+        val materialButton = onView(
+            allOf(
+                withId(R.id.button), withText("New"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(android.R.id.content),
+                        0
+                    ),
+                    10
+                ),
+                isDisplayed()
+            )
+        )
+        materialButton.perform(click())
+
+        val materialButton2 = onView(
+            allOf(
+                withId(R.id.button_create), withText("Create"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(android.R.id.content),
+                        0
+                    ),
+                    8
+                ),
+                isDisplayed()
+            )
+        )
+        materialButton2.perform(click())
+
+        val appCompatImageButton = onView(
+            allOf(
+                withId(R.id.button_undo),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.linear),
+                        1
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        appCompatImageButton.perform(click())
+    }
+
+    private fun childAtPosition(
+        parentMatcher: Matcher<View>, position: Int
+    ): Matcher<View> {
+
+        return object : TypeSafeMatcher<View>() {
+            override fun describeTo(description: Description) {
+                description.appendText("Child at position $position in parent ")
+                parentMatcher.describeTo(description)
+            }
+
+            public override fun matchesSafely(view: View): Boolean {
+                val parent = view.parent
+                return parent is ViewGroup && parentMatcher.matches(parent)
+                        && view == parent.getChildAt(position)
+            }
+        }
+    }
+}
---
+++ app/src/test/java/com/example/teamdolphin/TysonsUnitTest.kt
@@ -0,0 +1,12 @@
+package com.example.teamdolphin
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class TysonsUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
