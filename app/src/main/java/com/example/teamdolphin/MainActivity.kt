4b072b24ee9ca852f87678c25a15ab0332f18e60
IshuSingh1 <psingh@email.sc.edu>
2022-01-26 21:17:36 -0500

    added a 'dummy' gallery view to homepage

--- app/src/main/java/com/example/teamdolphin/MainActivity.kt
+++ app/src/main/java/com/example/teamdolphin/MainActivity.kt
@@ -1,9 +1,15 @@
 package com.example.teamdolphin
 
+import android.content.Context
 import android.content.Intent
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.*
+
 //This is the Home Page
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +22,74 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, FileCreation::class.java)
             startActivity(intent)
         }
+
+        var projectList = ArrayList<ProjectFile>()
+
+        //Currently these are dummy objects that you can see in the homepage
+        var names = arrayOf(
+            "image1aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", "image2", "image3","image1", "image2", "image3","image1", "image2", "image3"
+        )
+
+        var images = intArrayOf(
+            R.drawable.image1,
+            R.drawable.image2,
+            R.drawable.image3,
+            R.drawable.image1,
+            R.drawable.image2,
+            R.drawable.image3,
+            R.drawable.image1,
+            R.drawable.image2,
+            R.drawable.image3,
+        )
+
+        //Loop that goes through names or images and adds images & name to projectList
+        for(i in names.indices){
+            projectList.add(ProjectFile(names[i], images[i]))
+        }
+
+
+        var customAdapter = CustomAdapter(projectList, this)
+
+        //This gridView exists in activity_main.xml
+        var gridView = findViewById<GridView>(R.id.homepage_gridView)
+
+        gridView.adapter= customAdapter
+    }
+
+
+    //Adapter that helps inflate a view
+    class CustomAdapter(
+        var viewProject: ArrayList<ProjectFile>,
+        var context: Context
+    ) : BaseAdapter(){
+
+        var layoutInflator = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+
+        override fun getCount(): Int {
+            return viewProject.size
+        }
+
+        override fun getItem(p0: Int): Any {
+            return viewProject[p0]
+        }
+
+        override fun getItemId(p0: Int): Long {
+            return p0.toLong()
+        }
+
+        override fun getView(p0: Int, p1: View?, p2: ViewGroup?): View {
+            var view = p1
+            if(view==null){
+                view = layoutInflator.inflate(R.layout.item, p2, false)
+            }
+            var imageName = view?.findViewById<TextView>(R.id.item_image_name)
+            var imageView = view?.findViewById<ImageView>(R.id.item_image)
+
+            imageName?.text = viewProject[p0].name
+            imageView?.setImageResource(viewProject[p0].image!!)
+
+            return  view!!
+        }
+
     }
 }
