5956568334631d0db56329cebabd14ef27b05242
IshuSingh1 <psingh@email.sc.edu>
2022-01-27 00:44:05 -0500

    our project images are showing on homepage now

--- app/src/main/java/com/example/teamdolphin/MainActivity.kt
+++ app/src/main/java/com/example/teamdolphin/MainActivity.kt
@@ -2,16 +2,32 @@ package com.example.teamdolphin
 
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.database.Cursor
+import android.graphics.BitmapFactory
 import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
+import androidx.core.app.ActivityCompat
+import androidx.core.graphics.createBitmap
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import java.util.jar.Manifest
+import android.widget.GridView
+
+
+
 
 //This is the Home Page
 class MainActivity : AppCompatActivity() {
+    lateinit var rs:Cursor
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -23,54 +39,47 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
-        var projectList = ArrayList<ProjectFile>()
-
-        //Currently these are dummy objects that you can see in the homepage
-        var names = arrayOf(
-            "image1aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", "image2", "image3","image1", "image2", "image3","image1", "image2", "image3"
-        )
-
-        var images = intArrayOf(
-            R.drawable.image1,
-            R.drawable.image2,
-            R.drawable.image3,
-            R.drawable.image1,
-            R.drawable.image2,
-            R.drawable.image3,
-            R.drawable.image1,
-            R.drawable.image2,
-            R.drawable.image3,
-        )
-
-        //Loop that goes through names or images and adds images & name to projectList
-        for(i in names.indices){
-            projectList.add(ProjectFile(names[i], images[i]))
+        if(ActivityCompat.checkSelfPermission(this, android.Manifest.permission.READ_EXTERNAL_STORAGE)!=PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, Array(1){android.Manifest.permission.READ_EXTERNAL_STORAGE}, 121)
         }
+        listImages()
+    }
 
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode==121 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+            listImages()
+    }
 
-        var customAdapter = CustomAdapter(projectList, this)
-
-        //This gridView exists in activity_main.xml
-        var gridView = findViewById<GridView>(R.id.homepage_gridView)
-
-        gridView.adapter= customAdapter
+    private fun listImages() {
+        var cols = listOf<String>(MediaStore.Images.Thumbnails.DATA).toTypedArray()
+        rs = contentResolver.query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,  cols,null,null, null)!!
+        if(rs?.moveToNext()!!)
+            Toast.makeText(applicationContext, rs?.getString(0), Toast.LENGTH_LONG).show()
+        var gridView: GridView = findViewById(R.id.homepage_gridView)
+        gridView.adapter = ImageAdapter(applicationContext)
+        print("12345")
     }
 
 
     //Adapter that helps inflate a view
-    class CustomAdapter(
-        var viewProject: ArrayList<ProjectFile>,
-        var context: Context
-    ) : BaseAdapter(){
+    inner class ImageAdapter : BaseAdapter{
+        lateinit var context: Context
 
-        var layoutInflator = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        constructor(context: Context){
+            this.context = context
+        }
 
         override fun getCount(): Int {
-            return viewProject.size
+            return rs.count
         }
 
         override fun getItem(p0: Int): Any {
-            return viewProject[p0]
+            return p0
         }
 
         override fun getItemId(p0: Int): Long {
@@ -78,17 +87,14 @@ class MainActivity : AppCompatActivity() {
         }
 
         override fun getView(p0: Int, p1: View?, p2: ViewGroup?): View {
-            var view = p1
-            if(view==null){
-                view = layoutInflator.inflate(R.layout.item, p2, false)
-            }
-            var imageName = view?.findViewById<TextView>(R.id.item_image_name)
-            var imageView = view?.findViewById<ImageView>(R.id.item_image)
-
-            imageName?.text = viewProject[p0].name
-            imageView?.setImageResource(viewProject[p0].image!!)
-
-            return  view!!
+            var iv = ImageView(context)
+            rs.moveToPosition(p0)
+            var path = rs.getString(0)
+            var bitmap = BitmapFactory.decodeFile(path)
+            iv.setImageBitmap(bitmap)
+            iv.setPadding(20, 20, 20, 20)
+            iv.layoutParams = AbsListView.LayoutParams(GridView.AUTO_FIT, 500)
+            return iv
         }
 
     }
